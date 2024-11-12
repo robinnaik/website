976b4fb58f8c18da03527813e485e11bcb384705
Ravin Naik <49035282+robinnaik@users.noreply.github.com>
2024-11-12 22:51:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 const App = () => {
   return (<>
-    <h1>Website with complete CI/CD from GitHub to Cloud Run!!</h1>
+    <h1>Website with complete CI/CD from GitHub to Cloud Run!!!</h1>
 <img src="https://storage.googleapis.com/my-photos-collection/Garba2024/IMG-20241006-WA0013.jpg" />
 </>
   );
