8e934690f6a9be48e09988e44739a20a20f58d73
robinnaik <49035282+robinnaik@users.noreply.github.com>
2025-02-28 13:29:55 +0530

    Replaced personal photo with dora research diagram

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 const App = () => {
   return (<>
     <h1>Website with complete CI/CD from GitHub to Cloud Run!!!</h1>
-<img src="https://storage.googleapis.com/my-photos-collection/Garba2024/IMG-20241006-WA0013.jpg" />
-</>
+    <img style={{ width: '100%' }} src="https://dora.dev/research/dora-core-v2.0.0-detail.png" />
+  </>
   );
 }
 
