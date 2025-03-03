ab1f3875ab24cba93391591caee05b3b00242dac
Ravin Naik <49035282+robinnaik@users.noreply.github.com>
2025-03-03 17:53:46 +0530

    added tag line

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 const App = () => {
   return (<>
     <h1>DevOps, SRE, DORA and so on...</h1>
+    <h3>Get Better at Getting Better!!!</h3>
     <img style={{ width: '100%' }} src="https://dora.dev/research/dora-core-v2.0.0-detail.png" />
   </>
   );
